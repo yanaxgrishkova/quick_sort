398f27f9cd568e841a4698c41eeb72e32f0b71f8
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-11 04:42:13 +0300

    Create quick_sort.hpp

---
+++ include/quick_sort.hpp
@@ -0,0 +1,109 @@
+#include <future>
+#include <lomuto_partition.hpp>
+#include <thread_safe_stack.hpp>
+#include <thread>
+
+
+template <typename BidirectionalIterator>
+class parallel_quick_sorter_t
+{
+private:
+	struct chunk_to_sort_t
+	{
+		BidirectionalIterator first;
+		BidirectionalIterator last;
+		std::promise<void> promise;
+	};
+  
+public:
+	parallel_quick_sorter_t();
+	parallel_quick_sorter_t(parallel_quick_sorter_t const & other) = delete;
+	auto operator =(parallel_quick_sorter_t const & other)->parallel_quick_sorter_t & = delete;
+	parallel_quick_sorter_t(parallel_quick_sorter_t && other) = delete;
+	auto operator =(parallel_quick_sorter_t && other)->parallel_quick_sorter_t & = delete;
+	~parallel_quick_sorter_t();
+
+	void do_sort(BidirectionalIterator first, BidirectionalIterator last);
+	void sort_thread();
+	void try_sort_chunk();
+	void sort_chunk(std::shared_ptr<chunk_to_sort_t> chunk);
+  
+private:
+	std::vector<std::thread> threads_;
+	unsigned int const max_threads_count_;
+	stack<chunk_to_sort_t> chunks_;
+	std::atomic<bool> end_of_data_;
+	mutable std::mutex mutex_;
+
+};
+
+template <typename BidirectionalIterator>
+parallel_quick_sorter_t<BidirectionalIterator>::parallel_quick_sorter_t() :
+	max_threads_count_{ std::thread::hardware_concurrency() - 1 },
+	end_of_data_{ false } {}
+
+template <typename BidirectionalIterator>
+parallel_quick_sorter_t<BidirectionalIterator>::
+~parallel_quick_sorter_t()
+{
+	end_of_data_ = true;
+	for (auto && thread : threads_)
+		threads_.join();
+}
+
+template <typename BidirectionalIterator>
+void parallel_quick_sorter_t<BidirectionalIterator>::
+do_sort(BidirectionalIterator first, BidirectionalIterator last)
+{
+	if (first == last)
+		return;
+    
+	auto part_= partition(first, last);
+	auto first_chunk = std::make_shared<chunk_to_sort_t>(chunk_to_sort_t(first, part_));
+	auto first_task = first_chunk->promise.get_future();
+	chunks_.push(first_chunk);
+  
+	do_sort(part_, last);
+
+	while (first_task.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready)
+		sort_thread();
+}
+
+template <typename BidirectionalIterator>
+void parallel_quick_sorter_t<BidirectionalIterator>::
+try_sort_chunk()
+{
+	auto chunk = chunks_.pop();
+	if (chunk)
+		sort_chunk(*chunk);
+}
+
+template <typename BidirectionalIterator>
+void parallel_quick_sorter_t<BidirectionalIterator>::
+sort_thread()
+{
+	while (!end_of_data_)
+	{
+		try_sort_chunk;
+		std::this_thread::yield();
+	}
+}
+
+template <typename BidirectionalIterator>
+void parallel_quick_sorter_t<BidirectionalIterator>::
+sort_chunk(std::shared_ptr<chunk_to_sort_t> chunk)
+{
+	do_sort(chunk->first, chunk->last);
+	chunk.promise.set_value();
+}
+
+template <typename BidirectionalIterator>
+void parallel_quick_sort(BidirectionalIterator first, BidirectionalIterator last)
+{
+	if (first == last)
+		return;
+
+	parallel_quick_sorter_t<BidirectionalIterator> sorter;
+
+	sorter.do_sort(first, last);
+}
