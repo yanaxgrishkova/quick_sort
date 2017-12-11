dde70aaf3eb9a833f5d7b6a4e8baf6d75a31664e
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-11 04:10:01 +0300

    Update and rename quick_sort.hpp to lomuto_partition.hpp

---
+++ include/lomuto_partition.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template <typename Iterator>
+void quickSort(Iterator first, Iterator last)
+{
+	if (first < last)
+	{
+		p = partition(first, last);
+		quickSort(first, p - 1);
+		quickSort(p + 1, last);
+	}
+}
+
+template <typename Iterator>
+void partition(Iterator first, Iterator last)
+{
+	auto pivot = *last;
+	Iterator i = first - 1;
+	for (Iterator j = first; j < last - 1; ++j)
+	{
+		if (*j <= pivot)
+		{
+			i++;
+			std::swap(*i, *j)
+		}
+	}
+	std::swap(*(i + 1), *last);
+	
+	return i + 1;
+}
